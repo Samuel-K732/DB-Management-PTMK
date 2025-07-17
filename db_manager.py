011ce32c32c647ba8e90f09f03122ce6edb1ad43
Samuel <125740724+Samuel-K732@users.noreply.github.com>
2025-07-17 12:48:24 +0300

    Initial commit

--- db_manager.py
+++ db_manager.py
@@ -4,22 +4,22 @@ from timer import perf_timer
 
 
 class DBManager:
-    def __init__(self, session):
+    def __init__(self, session: Session) -> None:
         self.session = session
 
-    def insert(self, obj):
+    def insert(self, obj: Employee) -> None:
         self.session.add(obj)
         self.session.commit()
         self.session.refresh(obj)
 
-    def get_age(self, birth_date):
+    def get_age(self, birth_date: date) -> int:
         today = date.today()
         age = today.year - birth_date.year
         if (today.month, today.day) < (birth_date.month, birth_date.day):
             age -= 1
         return age
 
-    def get_unique_employees(self):
+    def get_unique_employees(self) -> list[Employee]:
         employees = self.session.query(Employee).order_by(Employee.full_name).all()
         keys = set()
         unique_employees = []
@@ -30,12 +30,12 @@ class DBManager:
                 keys.add(key)
         return unique_employees
 
-    def batch_insert(self, employees, batch_size=10000):
+    def batch_insert(self, employees: list[Employee], batch_size: int = 10000) -> None:
         for i in range(0, len(employees), batch_size):
             batch = employees[i:i + batch_size]
             self.session.add_all(batch)
             self.session.commit()
 
     @perf_timer
-    def get_f_males(self):
+    def get_f_males(self) -> list[Employee]:
         return self.session.query(Employee).filter(Employee.gender == "Male", Employee.full_name.startswith("F")).all()
